305dc6f2c7c56a7bcca27fcc7fd8a315582369bc
Elactix-Nova <31545372+Elactix-Nova@users.noreply.github.com>
2022-04-25 12:38:28 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,6 +1,6 @@
 const config = require("./config");
 const { app, logger } = require("./server");
 
-app.listen(config.port, '0.0.0.0', () => {
-  logger.info(`Listening on http://0.0.0.0:${config.port}`);
+app.listen(config.port, () => {
+  logger.info(`Listening on http://localhost:${config.port}`);
 });
